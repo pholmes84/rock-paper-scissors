db37d2769aa9c5df83a4ea984e42fb0d3ab3cd00
pholmes84 <theviolist57@gmail.com>
2020-12-18 16:04:47 -0600

    add scorekeeping logic to the game

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -16,40 +16,48 @@ function computerPlay() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-/* Returns a winner for a round of rock, paper, scissors.                    */
+/* Returns a winner for a round of rock, paper, scissors. 0 is a tie,        */
+/* 1 is the player, and two is the computer                                  */
 function playRound(playerSelection, computerSelection) {
 
-    let winnerText;
+    let winner = ["", 0];
+
 
     if (playerSelection === computerSelection) {
-        winnerText = "It was a tie :/ Try again.";
+        winner[0]= "It was a tie :/ Try again.";
     }
     else if (playerSelection === "R") {
         if (computerSelection === "P") {
-            winnerText = "You Lose :( Paper beats Rock."
+            winner[0] = "You Lose :( Paper beats Rock."
+            winner[1] = 2;
         }
         else {
-            winnerText = "You Win! Rock beats Scissors.";
+            winner[0] = "You Win! Rock beats Scissors.";
+            winner[1] = 1;
         }
     }
     else if (playerSelection === "P") {
         if (computerSelection === "S") {
-            winnerText = "You Lose :( Scissors beats Paper."
+            winner[0] = "You Lose :( Scissors beats Paper."
+            winner[1] = 2;
         }
         else {
-            winnerText = "You Win! Paper beats Rock.";
+            winner[0] = "You Win! Paper beats Rock.";
+            winner[1] = 1;
         }
     }
     else if (playerSelection === "S") {
         if (computerSelection === "R") {
-            winnerText = "You Lose :( Rock beats Scissors."
+            winner[0] = "You Lose :( Rock beats Scissors."
+            winner[1] = 2;
         }
         else {
-            winnerText = "You Win! Scissors beats Rock.";
+            winner[0] = "You Win! Scissors beats Rock.";
+            winner[1] = 1;
         }
     }
 
-    return winnerText;
+    return winner;
 }
 
 /* Gets the player's choice and returns the first character                  */
@@ -61,10 +69,37 @@ function getPlayerSelection() {
     return playerSelection.charAt(0).toUpperCase();
 }
 
+let score = [0, 0];
+
 /* plays 5 rounds of Rock, Paper, Scissors with the computer                 */
 function game() {
     for (let i = 1; i <= 5; i++) {
-        console.log(playRound(getPlayerSelection(), computerPlay()));
+        //Store player(score[0]) and computer(score[1]) scores
+
+        let playerSelection = getPlayerSelection();
+        let computerSelection = computerPlay();
+
+        let winner = playRound(playerSelection, computerSelection);
+
+        if (winner[1] === 1) {
+            score[0]++;
+        }
+        else if (winner[1] === 2) {
+            score[1]++;
+        }
+
+        console.log(winner[0]);
+        console.log("You: " + score[0] + "\nComputer: " + score[1]);
+    }
+
+    if (score[0] > score[1]) {
+        console.log("You win the game!");
+    }
+    else if (score[0] < score[1]) {
+        console.log("You lost the game :(");
+    }
+    else {
+        console.log("It was a tie :/");
     }
 }
 
